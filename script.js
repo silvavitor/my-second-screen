2e36fa8621ceb4248c7ac8cf574e842bfb86df2a
Vitor <vitor_fernando@outlook.com.br>
2020-10-03 20:06:25 -0300

    v0.1 second screen files

---
+++ script.js
@@ -0,0 +1,31 @@
+const videoElement = document.querySelector("#video");
+const startButton = document.querySelector("#start");
+const stopButton = document.querySelector("#stop");
+
+async function displayPictureInPicture() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = async () => {
+            videoElement.play();
+            await videoElement.requestPictureInPicture();
+        }
+    } catch (error) {
+        console.log("error: ", error);
+    }
+}
+
+startButton.addEventListener("click", async () => {
+    await displayPictureInPicture();
+    startButton.hidden = true;
+    stopButton.hidden = false;
+});
+
+stopButton.addEventListener("click", async () => {
+    if(document.pictureInPictureElement){
+        await document.exitPictureInPicture();
+    }
+    stopButton.hidden = true;
+    startButton.hidden = false;
+});
+
